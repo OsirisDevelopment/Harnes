3ebc10adf7b66819bdaa367048405a738768b96c
developscrum <paolo.caviedes@gmail.com>
2016-03-28 12:10:29 -0400

    ejemplo de response
    Enviado por manoj sharma

---
+++ log manoj.py
@@ -0,0 +1,14 @@
+{
+    "timestamp": "2016-03-23T19:53:33",
+    "token": "b5811723",
+    "tag": "logs.netflows.zeushack100-b5811723",
+    "port_src": 44880,
+    "tos": 0,
+    "ip_dst": "107.170.193.109",
+    "bytes": 1209,
+    "port_dst": 8889,
+    "packets": 22,
+    "ip_proto": "tcp",
+    "geoip.location": "37.42499923706055,-121.94599914550781",
+    "ip_src": "128.107.241.188"
+  }
